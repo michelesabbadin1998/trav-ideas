ce1a9653eeb26f17baf4f4b7569ac8c9fcffd6a9
michelesabbadin1998 <138523656+michelesabbadin1998@users.noreply.github.com>
2024-10-17 10:21:40 +0200

    Delete PAG_CONTINENTI/AFRICA/script.js

--- PAG_CONTINENTI/AFRICA/script.js
+++
@@ -1,9 +0,0 @@
-// script.js
-function toggleDetails(event) {
-    event.stopPropagation(); // Previene che il click attivi anche il redirect
-    const dettagli = event.target.closest('.proposta').querySelector('.proposta-dettagli');
-    dettagli.classList.toggle('active');
-    
-    // Ruota la freccia
-    event.target.classList.toggle('rotated');
-}
